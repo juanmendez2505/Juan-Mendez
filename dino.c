2f5dc0091e6e98fd4446d2e78595289b75657b63
juanmendez2505 <144180777+juanmendez2505@users.noreply.github.com>
2023-10-31 17:01:46 -0500

    Add files via upload

---
+++ dino.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h> // Incluye la biblioteca stdbool.h
+
+int main() {
+    bool esMayorDeEdad = false; // Declaración y asignación de una variable bool
+
+    if (esMayorDeEdad) 
+    {
+        printf("La persona es mayor de edad.\n");
+    } else {
+        printf("La persona es menor de edad.\n");
+    }
+
+    return 0;
+}
